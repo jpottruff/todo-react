0c1db751f5e2868e48a83a2f639e55336ec417f6
Jeff Pottruff <jeffpottruff@gmail.com>
2023-04-19 19:34:10 -0400

    move list to own component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import TodoList from "./components/TodoList";
+import "./App.css";
 
 function App() {
   return (
     <div>
       <h1 className="title">Todo List React</h1>
-      <ul className="todo-list">
-        <li>todo 1</li>
-        <li>todo 2</li>
-        <li>todo 3</li>
-      </ul>
+      <TodoList />
     </div>
   );
 }
---
+++ src/components/TodoList.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./TodoList.css";
+
+const TodoList = () => {
+  return (
+    <ul className="todo-list">
+      <li>todo 1</li>
+      <li>todo 2</li>
+      <li>todo 3</li>
+    </ul>
+  );
+};
+
+export default TodoList;
