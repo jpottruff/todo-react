88df0a3bedb467742c996ffebaa232ca82597743
Jeff Pottruff <jeffpottruff@gmail.com>
2023-04-30 10:19:07 -0400

    handle no todos

--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -2,6 +2,10 @@ import React from "react";
 import "./TodoList.css";
 
 const TodoList = (props) => {
+  if (props.items.length === 0) {
+    return <h2 className="no-items-msg">No Items</h2>;
+  }
+
   return (
     <ul className="todo-list">
       {props.items.map((item) => {
